f565c7076fc0b7d05edaaf076d466d880a3b1442
Niraj Fonseka <fonseka.live@utexas.edu>
2018-05-25 04:39:05 +0000

    Adding Raspberry Pi configs

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash          
 echo Hello World  
 
-base64 dailyReport.txt -D > dest_audio_file.mp3
+base64 dailyReport.txt -d > dest_audio_file.mp3
 
-play dest_audio_file.mp3
+omxplayer dest_audio_file.mp3
